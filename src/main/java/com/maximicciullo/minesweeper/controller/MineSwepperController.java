f8092b3454830949572951ea170ef15016873ab9
maxi micciullo <max.micciullo@appdirect.com>
2019-10-13 14:07:13 -0300

    Removed not necessary welcome endpoint and also fixed path in flag and question endpoints

--- src/main/java/com/maximicciullo/minesweeper/controller/MineSwepperController.java
+++ src/main/java/com/maximicciullo/minesweeper/controller/MineSwepperController.java
@@ -3,29 +3,16 @@ package com.maximicciullo.minesweeper.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maximicciullo.minesweeper.model.MineSweeperWelcomeBean;
-
 @RestController
 @RequestMapping("/mineswepper/v1")
 @Validated
 public class MineSwepperController {
-
-	@GetMapping(value="/welcome")
-	public ResponseEntity welcome() {
-		try {
-			MineSweeperWelcomeBean welcome = new MineSweeperWelcomeBean();
-			return ResponseEntity.ok(welcome.getMessage());
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-		}
-	}
 
 	@PostMapping(value="/game")
 	public ResponseEntity newGame() {
@@ -51,7 +38,7 @@ public class MineSwepperController {
 		}
 	}
 
-	@PutMapping(value = "/game/flag/{userName}")
+	@PutMapping(value = "/game/{userName}/flag")
 	public ResponseEntity redFlag() {
 		try {
 			// Get user name in the url path
@@ -62,7 +49,7 @@ public class MineSwepperController {
 		}
 	}
 
-	@PutMapping(value = "/game/question/{userName}")
+	@PutMapping(value = "/game/{userName}/question")
 	public ResponseEntity questionMark() {
 		try {
 			// Get user name in the url path
